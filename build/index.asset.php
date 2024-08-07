949cc2a5560b5802523a14b56f3bcce1d44d4201
Rahat Baksh <rahat392@gmail.com>
2024-08-07 16:58:57 +0600

    updating card styles

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '77a4214773879dd246c7');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '4697c54bfb184c7e1768');
